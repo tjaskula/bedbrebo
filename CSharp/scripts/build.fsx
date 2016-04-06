8a9c9cdd59304c505428c5e824054d8b96726ec9
Jaskula Tomasz <thomasv1000@hotmail.fr>
2016-04-06 23:30:06 +0200

    Change path build.fsx

--- CSharp/scripts/build.fsx
+++ CSharp/scripts/build.fsx
@@ -1,5 +1,5 @@
 ﻿// sets the current directory to be same as the script directory
-System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__ + "\..\src")
+System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__ + "/../src")
 
 #I "../src/packages/FAKE/tools/"
 #r "FakeLib.dll"
