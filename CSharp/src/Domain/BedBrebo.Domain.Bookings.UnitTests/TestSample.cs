8856d4af4df0a1344347611ec6d5126a9551c8c5
Jaskula Tomasz <thomasv1000@hotmail.fr>
2016-06-08 18:13:42 +0200

    Add a simple UT with xUnit package

---
+++ CSharp/src/Domain/BedBrebo.Domain.Bookings.UnitTests/TestSample.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace BedBrebo.Domain.Bookings.UnitTests
+{
+    public class TestSample
+    {
+        [Fact]
+        public void Test()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
