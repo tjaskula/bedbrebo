1c0e885a09ce8b24854c4fe0c3b11baf2fbc62f0
Jaskula Tomasz <thomasv1000@hotmail.fr>
2016-06-08 17:27:19 +0200

    Fixing AssemblyInfo

--- CSharp/src/Domain/BedBrebo.Domain.Bookings/Properties/AssemblyInfo.cs
+++ CSharp/src/Domain/BedBrebo.Domain.Bookings/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Domain")]
+[assembly: AssemblyTitle("BedBrebo.Domain.Bookings")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("Domain")]
+[assembly: AssemblyProduct("BedBrebo.Domain.Bookings")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
