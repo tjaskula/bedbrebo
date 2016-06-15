ed3215eb663fdbac0d5523c57ddf391cd0bc772f
Jaskula Tomasz <thomasv1000@hotmail.fr>
2016-06-15 11:22:42 +0200

    Add constructors to InvalidIdentifierAssignment to comply to the .net pattern.

--- CSharp/src/Domain/BedBrebo.Domain.Core/Exceptions/InvalidIdentifierAssignment.cs
+++ CSharp/src/Domain/BedBrebo.Domain.Core/Exceptions/InvalidIdentifierAssignment.cs
@@ -1,10 +1,23 @@
 using System;
+using System.Runtime.Serialization;
 
 namespace BedBrebo.Domain.Core.Exceptions
 {
     public class InvalidIdentifierAssignment : Exception
     {
         public InvalidIdentifierAssignment(string message) : base(message)
+        {
+        }
+
+        public InvalidIdentifierAssignment(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        public InvalidIdentifierAssignment()
+        {
+        }
+
+        public InvalidIdentifierAssignment(SerializationInfo info, StreamingContext context) : base(info, context)
         {
         }
     }
