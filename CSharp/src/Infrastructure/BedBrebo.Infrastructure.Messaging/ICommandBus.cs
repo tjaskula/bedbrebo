3c687aea0981690c9be28b11d30d8b49a413e57f
Thomas Jaskula <thomasv1000@hotmail.fr>
2016-08-04 18:06:49 +0200

    Add BedBrebo.Infrastructure.Messaging + ICommandBus interface

---
+++ CSharp/src/Infrastructure/BedBrebo.Infrastructure.Messaging/ICommandBus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using BedBrebo.Domain.Core;
+
+namespace BedBrebo.Infrastructure.Messaging
+{
+    public interface ICommandBus
+    {
+        void Send(ICommand command);
+        void Send(ICommand command, Action onSuccess, Action<Exception> onError = null);
+    }
+}
